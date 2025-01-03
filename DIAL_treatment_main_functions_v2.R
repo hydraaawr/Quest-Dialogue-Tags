f3a1012f3f1aaf8c982852febd10159ed801a51b
hydraaawr <hydraaawr@gmail.com>
2025-01-03 15:21:00 +0100

    3.0.0gen - Fixed DIAL FULL formids masking INFO RNAM issue (skyrim and dawnguard 1.1.0beta

--- DIAL_treatment_main_functions_v2.R
+++ DIAL_treatment_main_functions_v2.R
@@ -20,20 +20,20 @@ shaper <- function(db_dial){
     ##  separate into 2 dbs because of record subrecord structure
 
     db_dial_qnam_skyrim.esm <- db_dial %>%
-    filter(!is.na(X.0...3.)) %>% ## contains formid 2, therefore contains FULL and QNAM
-        rename(Formid = X.0...3.) %>%
-        select(Record,Formid,FULL,QNAM)
+    filter(!is.na(X.0...3.)) %>% ## contains Formid_DIAL 2, therefore contains FULL and QNAM
+        rename(Formid_DIAL = X.0...3.) %>%
+        select(Record,Formid_DIAL,FULL,QNAM)
 
     db_dial_info_skyrim.esm <- db_dial %>%
-    filter(!is.na(X.0.)) %>% ## contains formid 1, therefore contains INFO,Scriptname, RNAM. FULL COLUMN IS A SEPARATOR aberration
-        rename(Formid = X.0.) %>%
-        select(Record,Formid,INFO,Scriptname,RNAM)
+    filter(!is.na(X.0.)) %>% ## contains Formid_DIAL 1, therefore contains INFO,Scriptname, RNAM. FULL COLUMN IS A SEPARATOR aberration
+        rename(Formid_DIAL = X.0.) %>%
+        select(Record,Formid_DIAL,INFO,Scriptname,RNAM)
 
     #########################
 
     ## join both
 
-    db_dial_merged <- full_join(db_dial_qnam_skyrim.esm,db_dial_info_skyrim.esm, by = "Formid") #relationship = "many-to-many")
+    db_dial_merged <- full_join(db_dial_qnam_skyrim.esm,db_dial_info_skyrim.esm, by = "Formid_DIAL") #relationship = "many-to-many")
 
     return(db_dial_merged)
 }
@@ -45,7 +45,6 @@ json_gen <- function(db_dial_json_ready,plugin,target_NA){
 
 
   json_obj_list <- vector("list", length(nrow(db_dial_json_ready)))
-  json_extra_obj_list <- vector("list") ## in case we need to save the FULL and RNAM of the same
 
 
 
@@ -55,7 +54,7 @@ json_gen <- function(db_dial_json_ready,plugin,target_NA){
 
       json_obj_list[[i]] <- { 
         list(
-          form_id = paste0(db_dial_json_ready[i,"Formid_isolated"],"|",plugin), 
+          form_id = paste0(db_dial_json_ready[i,"Formid_DIAL_isolated"],"|",plugin), 
           type = "DIAL FULL",     
           string = db_dial_json_ready[i,"FULL_trans"]    
         )
@@ -64,7 +63,7 @@ json_gen <- function(db_dial_json_ready,plugin,target_NA){
     } else if(identical(db_dial_json_ready[i,"FULL_trans"],target_NA) && !identical(db_dial_json_ready[i,"RNAM_trans"],target_NA)) { ## opposite of previous
       json_obj_list[[i]] <- { 
         list(
-          form_id = paste0(db_dial_json_ready[i,"Formid_isolated"],"|",plugin), 
+          form_id = paste0(db_dial_json_ready[i,"Formid_DIAL_isolated"],"|",plugin), 
           type = "DIAL FULL",     
           string = db_dial_json_ready[i,"RNAM_trans"]    
         )
@@ -75,25 +74,15 @@ json_gen <- function(db_dial_json_ready,plugin,target_NA){
 
       json_obj_list[[i]] <- { 
         list(
-          form_id = paste0(db_dial_json_ready[i,"Formid_isolated"],"|",plugin), 
-          type = "DIAL FULL",     
-          string = db_dial_json_ready[i,"FULL_trans"]    
-        )
-      }
-
-      json_extra_obj_list[[i]] <- { 
-        list(
-          form_id = paste0(db_dial_json_ready[i,"Formid_isolated"],"|",plugin), 
-          type = "DIAL FULL",     
+          form_id = paste0(db_dial_json_ready[i,"Formid_INFO_isolated"],"|",plugin), 
+          type = "INFO RNAM",     
           string = db_dial_json_ready[i,"RNAM_trans"]    
         )
       }
+
     }
   }
 
-  json_extra_obj_list <- json_extra_obj_list[lengths(json_extra_obj_list) != 0] ## clean NULL objects
-  ## print(length(json_extra_obj_list))
-  json_obj_list <- append(json_obj_list,json_extra_obj_list) ## join both
   json_obj_list <- json_obj_list[!duplicated(json_obj_list)] ## cleaning duplicates 
   json_output <- toJSON(json_obj_list, pretty = TRUE, auto_unbox = TRUE)
 
