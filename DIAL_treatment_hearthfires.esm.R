7ad8abe343b8174a29c1f1377e1da5e68a0842ff
hydraaawr <hydraaawr@gmail.com>
2025-09-05 15:03:12 +0200

    hearthfires.esm - implemented filter_rejection and rm_na_renamer functions. No json changes

--- DIAL_treatment_hearthfires.esm.R
+++ DIAL_treatment_hearthfires.esm.R
@@ -7,7 +7,7 @@ rm(list = ls())
 
 ## Loading
 
-source(".\\DIAL_treatment_functions_v3.0.R")
+source(".\\DIAL_treatment_functions_v4.0.R")
 db_dial_hearthfires.esm <- read.csv(".\\dbs\\db_DIAL_hearthfires.esm.csv", sep = ";")
 
 
@@ -28,6 +28,17 @@ db_dial_hearthfires.esm_massclass <- db_dial_hearthfires.esm_merged %>%
 
 ## ready for json db (filtering and adding tags)
 
+rejection_vector_hearthfires.esm <- c(
+  paste(
+    "never mind",
+    "i'll have to think about it",
+    sep = "|"
+  )
+)
+
+
+
+
 db_dial_hearthfires.esm_json_ready <- db_dial_hearthfires.esm_massclass %>%
   isolate_ids() %>%
       filter(
@@ -38,22 +49,8 @@ db_dial_hearthfires.esm_json_ready <- db_dial_hearthfires.esm_massclass %>%
             # Ensure at least one of RNAM or FULL has a value
             !is.na(RNAM) | !is.na(FULL)
           ) %>%
-            filter(
-                # Remove entries with rejection phrases because those might or might not contain scriptname
-              !(
-                # Check FULL (managing NA)
-                if_else(is.na(FULL), FALSE, 
-                      str_detect(FULL, regex("(?i)never mind|i'll have to think about it", ignore_case = TRUE))) |
-                # Check RNAM (managing NA)  
-                if_else(is.na(RNAM), FALSE,
-                      str_detect(RNAM, regex("(?i)never mind|i'll have to think about it", ignore_case = TRUE)))
-                  )) %>% 
-            mutate(
-              # Replace any RNAM containing "TIF_" with "NA (Quest)"
-              RNAM = if_else(str_detect(RNAM, "TIF_"), "NA", RNAM),
-              FULL_trans = paste0(FULL, " (Quest)"), ## Add "(Quest)"
-              RNAM_trans = paste0(RNAM, " (Quest)")
-            )
+            filter_rejection_phrases(rejection_vector_hearthfires.esm) %>%
+              rm_na_renamer_full_rnam()
 
 
 
