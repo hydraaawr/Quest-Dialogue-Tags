4867338657f7ba2a5bba546447b97cb9a3ccaab0
hydraaawr <hydraaawr@gmail.com>
2025-09-05 16:33:14 +0200

    main - implemented all massclassers of components via workaround

--- DIAL_treatment_main.R
+++ DIAL_treatment_main.R
@@ -29,43 +29,16 @@ db_dial_ussep_merged <- shaper(db_dial_ussep)
 ## db with massively classified QNAM
 
 db_dial_ussep_massclass <- db_dial_ussep_merged %>%
-    mutate( ## clasify type of quest
-        QNAM_type = case_when(
-        ## skyrim.esm
-          str_detect(QNAM, "^MQ") ~ "MQ", ## Main Quest
-          str_detect(QNAM, "^MG") ~ "MG", ## Mages guild
-          str_detect(QNAM, "^C\\d{2}|CR") ~ "C", ## Companions
-          str_detect(QNAM, "^DB") ~ "DB", ## Dark brotherhood
-          str_detect(QNAM, "^CW") ~ "CW", ## Civil war(legion + stormcloacks)
-          str_detect(QNAM, "^TG") ~ "TG", ## Thieves guild
-          str_detect(QNAM, "^DA\\d{2}") ~ "DA", ## Daedric
-          str_detect(QNAM, "^MS\\d{2}|^VC\\d{2}|^dun|^NN\\d{2}|^[Tt]\\d{2}") ~ "MS", ## Side quests
-          str_detect(QNAM, "Favor|Freeform|^Tutorial|BQ|Farm|City Dialogue") ~ "misc", ## Miscellaneous
-          str_detect(Formid_DIAL,"Heard any rumors lately?|What's the word around town?") ~ "rumor_skyrim", ## rumors
-        ## dawnguard.esm
-          str_detect(QNAM, "DLC1VQ01MiscObjective|DLC1VQ01|DLC1VQ02|DLC1HunterBaseIntro|DLC1VQ03Hunter|DLC1VampireBaseIntro|DLC1VQ03Vampire|DLC1VQElder|DLC1VQElderHandler|DLC1VQ04|DLC1VQ05|DLC1VQ06|DLC1VQ07|DLC1VQ08") ~ "MQ", ## Main Quest
-          str_detect(QNAM, "DLC1RH") ~ "DG", ## dawnguard radiants
-          str_detect(QNAM, "DLC1RV") ~ "VP", ## vampire radiants
-          str_detect(QNAM, "DLC1VQSaint|DLC1LD") ~ "SQ", ## Side Quests
-          str_detect(QNAM, "DLC1VQDragon|DLC1VQFVBooks|DLC01SoulCairnHorseQuest2|DLC1Surgery") ~ "misc", ## Misc
-          str_detect(QNAM, "DLC1VampireTutorial") ~ "VT",
-        ## dragonborn.esm
-          str_detect(QNAM, "DLC2MQ01|DLC2WE09|DLC2MQ02|DLC2MQ03|DLC2MQ03B|DLC2MQ04|DLC2MQ05|DLC2MQ06") ~ "MQ", ## Main Quest
-          str_detect(QNAM, "DLC2BlackBook04Quest|DLC2RR03|DLC2RR01|DLC2TT1b|DLC2RR02") ~ "RRSD", ## Raven Rock Side Quests
-          str_detect(QNAM, "DLC2RR03Intro|DLC2RRFavor03|DLC2RRFavor06|DLC2RRFavor02|DLC2RRFavor07|DLC2RRFavor04|DLC2TGQuest|DLC2RRFavor05|DLC2RRFavor01") ~ "RRmisc", ## Raven Rock miscellaneous
-          str_detect(QNAM, "DLC2SV01|DLC2SV02|DLC2SV02Misc|DLC2WB01") ~ "RRSV", ## Skaal Village Side Quests
-          str_detect(QNAM, "DLC2SkaalVillageFreeform2|Favor104|DLC2SkaalVillageFreeform1") ~ "SVmisc", ## Skaal Village miscellaneous
-          str_detect(QNAM, "DLC2TTR7|DLC2BlackBook07Quest|DLC2TTR5|DLC2TTR4a|DLC2TTF1|DLC2TTF2|DLC2TTR2|DLC2TTR1|DLC2TT2|DLC2TT1|DLC2TTR3a|DLC2TTR3b") ~ "TMSD", ## Tel Mithryn Side quests
-          str_detect(QNAM, "DLC2TTR4b|DLC2TTR8") ~ "TMmisc", ## Tel Mithryn miscellaneous
-          str_detect(QNAM, "DLC2MH02|DLC2MH01") ~ "TMHSD", ## Thirsk Mead Hall Side quests
-          str_detect(QNAM, "DLC2ThirskFFElmusBack|DLC2ThirskFFElmus|DLC2ThirskFFHalbarn|DLC2ThirskFFHilund") ~ "TMHmisc", ## Thirsk Mead Hall miscellaneous
-          str_detect(QNAM, "DLC2BlackBook06Quest|DLC2BlackBook05Quest|DLC2BlackBook03Quest|DLC2dunHaknirTreasureQST|DLC2dunHaknirTreasureQSTMisc|DLC2EbonyWarriorQuest|DLC2dunKolbjornQST|DLC2dunKolbjornMiscQST") ~ "OS", ## Other Side
-          str_detect(QNAM, "DLC2WE06|DLC2dunHrodulf|DLC2KagrumezQST|DLC2SV02AncarionMerchant|DLC2dunKolbjornMiscQST|DLC2dunKarstaagQST|DLC2dunFrostmoonQSTMisc") ~ "misc", ## other miscellaneous quests
-          str_detect(Formid_DIAL,"Rumor") ~ "rumor_dragonborn", ## rumors
-        ## hearthfires.esm
-          str_detect(QNAM, "^BYOHHouse") ~ "house_hearthfires" ## Main Quest
-        )
-      )
+  mutate( ## work independently
+    QNAM_type_skyrim = massclasser_skyrim.esm(.)$QNAM_type,
+    QNAM_type_dawnguard = massclasser_dawnguard.esm(.)$QNAM_type,
+    QNAM_type_dragonborn = massclasser_dragonborn.esm(.)$QNAM_type,
+    QNAM_type_hearthfires = massclasser_hearthfires.esm(.)$QNAM_type
+  ) %>%
+  mutate( # to then merge, so all massclasser work as if they worked simultaneously
+    QNAM_type = coalesce(QNAM_type_skyrim, QNAM_type_dawnguard, QNAM_type_dragonborn, QNAM_type_hearthfires)
+  ) %>%
+  select(-QNAM_type_skyrim, -QNAM_type_dawnguard, -QNAM_type_dragonborn, -QNAM_type_hearthfires)
       
 
 
